ac2c188c72921c8e9f67854ae58e6f74fc86c6cd
imran-ali969 <buzzyfuzzy19@gmail.com>
2026-03-14 07:35:34 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

--- refinement_practice/task2_cwss.py
+++ refinement_practice/task2_cwss.py
@@ -6,13 +6,13 @@
 # Lately there has been a change in the ERP rates at 5 gantries across some expressways. 
 
 # The following program calculates the change in rates of these 5 gantries. 
-num = int(input("Enter number of gantries: "))
-for i in range(num): 
-    expressway = input("Enter name of gantry:") 
-    old = float(input("Enter old rate:")) 
-    new = float(input("Enter new rate:")) 
-    change = new - old 
-    print("Change is",change) 
+
+#for i in range(5): 
+#    expressway = input("Enter name of gantry:") 
+#    old = float(input("Enter old rate:")) 
+#    new = float(input("Enter new rate:")) 
+#    change = new - old 
+#    print("Change is",change) 
 
 ###########################################################
 # 6. Edit the program so that: 
@@ -20,6 +20,13 @@ for i in range(num):
 #       The program must display a suitable input message. [1] 
 ###########################################################
 # Copy + Paste & Write your code here
+#num = int(input("Enter number of gantries: "))
+#for i in range(num): 
+ #   expressway = input("Enter name of gantry:") 
+##    old = float(input("Enter old rate:")) 
+ #   new = float(input("Enter new rate:")) 
+ #   change = new - old 
+  #  print("Change is",change) 
 
 
 
@@ -32,7 +39,24 @@ for i in range(num):
 #       must loop until the name of the gantry is an input of a maximum of 20 letters. [4] 
 ###########################################################
 # Copy + Paste & Write your code here
+#check for length >> len()
+#check for letters >> .isalpha()
+#while True validation
+# num = int(input("Enter number of gantries: "))
+# for i in range(num): 
 
+#     while True:
+#         expressway = input("Enter name of gantry:") 
+#         if len(expressway) > 20:
+#             print("Expressway name cannot be more than 20.")
+#         elif expressway.isaplha() == False:
+#             print("Expressway name can only contain letters.")
+#         else:
+#             break
+#         old = float(input("Enter old rate:")) 
+#         new = float(input("Enter new rate:")) 
+#         change = new - old 
+#         print("Change is",change) 
 
 
 
@@ -41,8 +65,25 @@ for i in range(num):
 #       been increased is stored in a list and then displayed. [4] 
 ###########################################################
 # Copy + Paste & Write your code here
+# gantries_increase = []
+# num = int(input("Enter number of gantries: "))
+# for i in range(num): 
 
-
+#     while True:
+#         expressway = input("Enter name of gantry:") 
+#         if len(expressway) > 20:
+#             print("Expressway name cannot be more than 20.")
+#         elif expressway.isalpha() == False:
+#             print("Expressway name can only contain letters.")
+#         else:
+#             break
+#     old = float(input("Enter old rate:")) 
+#     new = float(input("Enter new rate:")) 
+#     change = new - old 
+#     print("Change is",change) 
+#     if change > 0:
+#          gantries_increase.append(expressway)
+# print(gantries_increase)
 
 
 ###########################################################
@@ -50,3 +91,27 @@ for i in range(num):
 #       in the ERP rate is displayed. [1] 
 ###########################################################
 # Copy + Paste & Write your code here
+gantries_increase = []
+num = int(input("Enter number of gantries: "))
+for i in range(num): 
+
+    while True:
+        expressway = input("Enter name of gantry:") 
+        if len(expressway) > 20:
+            print("Expressway name cannot be more than 20.")
+        elif expressway.isalpha() == False:
+            print("Expressway name can only contain letters.")
+        else:
+            break
+    old = float(input("Enter old rate:")) 
+    new = float(input("Enter new rate:")) 
+    change = new - old 
+    print("Change is",change) 
+    if change > 0:
+         gantries_increase.append(expressway)
+print(gantries_increase)
+# name = "john"
+# f"{name} has a dog."
+# f"THe length of john's name is {len(name)}"
+
+print(f"{len(gantries_increase)} is the number of gantries that increased")
