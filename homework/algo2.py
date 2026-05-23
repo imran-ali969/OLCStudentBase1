1e17d1f9a947cb1638e8c07f3791820aa7b6f0e8
imran-ali969 <buzzyfuzzy19@gmail.com>
2026-05-23 07:31:25 +0000

    2026-23-05

---
+++ homework/algo2.py
@@ -0,0 +1,43 @@
+##################################
+# Assignment
+# Scenario: Identifying Star Students for an Award Ceremony
+    
+# Your school is preparing for the end-of-year award ceremony.
+# The principal wants to recognise "Star Students" — those who have shown:
+# - Excellent attendance (at least 90%)
+# - Strong academic performance (score of at least 85)
+
+# You're given two separate dictionaries:
+# - One with student attendance (in %)
+# - One with student scores (out of 100)
+
+# Extract the names of students who meet both criteria.
+# (1) Store them in a list called star_students.
+    
+# (2) Loop through the star_students list and 
+#      print a congratulatory message to each star student like this:
+# "Congratulations Chloe Lim! With an attendance of 95% and a score of 92,
+# you are awarded the Star Student Award!""
+
+# You may assume that the same keys exist in both dictionaries.
+
+attendance = {
+    "Alex Tan": 92, "Benjamin Lee": 88, "Chloe Lim": 95,
+    "Dana Ong": 87, "Ethan Raj": 91, "Farah Bte Ahmad": 94,
+    "Gerald Chan": 78, "Hannah Goh": 85, "Ivan Wong": 96,
+    "Jolene Ng": 93, "Kishore Menon": 70, "Liyana Yusof": 90
+}
+
+scores = {
+    "Alex Tan": 84, "Benjamin Lee": 86, "Chloe Lim": 92,
+    "Dana Ong": 81, "Ethan Raj": 89, "Farah Bte Ahmad": 90,
+    "Gerald Chan": 65, "Hannah Goh": 88, "Ivan Wong": 93,
+    "Jolene Ng": 79, "Kishore Menon": 72, "Liyana Yusof": 85
+}
+star_attendance = []
+for names in attendance:
+    if attendance[names] >= 90 and scores[names] >= 85:
+        star_attendance.append(names)
+print(star_attendance)
+for names in star_attendance:
+    print(f"Congratulations {names}! With an attendance of {attendance[names]} and a score of {scores[names]}, you are awarded the Star Student Award!")
---
+++ homework/algo3.py
@@ -0,0 +1,37 @@
+###########################################################
+# Scenario 6: Processing Customer Data from a CRM System
+
+# You are working on a Customer Relationship Management (CRM) system 
+# that stores customer records as a single raw string.
+
+# Each record contains:
+# - Customer Name followed by a semicolon (;).
+# - Phone Number (8-digit number) immediately after the semicolon.
+# - Records are separated by a comma (,).
+
+# Your task is to:
+# - Part 1: Extract individual customer records from the raw string 
+#           and store them in a list.
+
+# expected output
+# ["Alice Johnson;98653263", "Catherine Lee;89653215", "Johnson Lam;95321453",
+#     "Brandon Tan;81234567", "Emily Wong;92345678", "Hafiz Zaid;85678912",
+#     "Isabelle Ng;96541234", "Ken Lim;97865432", "Noraini Ahmad;88776655",
+#     "Samuel Chan;90123456"]
+
+
+# - Part 2: Convert the list in part 1 into a dictionary, where:
+# -   Customer names are the keys.
+# -   Phone numbers are stored as integer values.
+
+# expected output
+# {"Alice Johnson": 98653263, "Catherine Lee": 89653215, "Johnson Lam": 95321453,
+#     "Brandon Tan": 81234567, "Emily Wong": 92345678, "Hafiz Zaid": 85678912,
+#     "Isabelle Ng": 96541234, "Ken Lim": 97865432, "Noraini Ahmad": 88776655,
+#     "Samuel Chan": 90123456 }
+
+customers = "Alice Johnson;98653263,Catherine Lee;89653215,Johnson Lam;95321453,Brandon Tan;81234567,Emily Wong;92345678,Hafiz Zaid;85678912,Isabelle Ng;96541234,Ken Lim;97865432,Noraini Ahmad;88776655,Samuel Chan;90123456"
+
+
+# write your code for part 1 here
+
